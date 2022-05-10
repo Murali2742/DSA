d19b54d75404997b5f730043535e82c1d70ac061
Murali Krishnan V <vmkrishnan2742@gmail.com>
2022-05-10 23:50:28 +0530

    Second Commit

---
+++ src/dsa/InsertionSort.java
@@ -0,0 +1,46 @@
+package dsa;
+import java.util.*;
+public class InsertionSort {
+
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        // 7 2 8 4 1
+        //
+        int temp=0;
+
+
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]<arr[i-1])
+            {
+                for(int j=i;j>=1;j--)
+                {
+                    if(arr[j]<arr[j-1])
+                    {
+                        temp=arr[j];
+                        arr[j]=arr[j-1];
+                        arr[j-1]=temp;
+                    }
+                }
+            }
+
+        }
+        for(int j=0;j<n;j++)
+        {
+            System.out.println(arr[j]);
+
+        }
+
+    }
+
+}
